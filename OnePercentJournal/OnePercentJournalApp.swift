1781aeacfd1ceb814b0359b1c7f4474335d85d15
Prachi Heda <prachiheda@gmail.com>
2025-02-12 21:51:38 -0800

    Initial Commit

---
+++ OnePercentJournal/OnePercentJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePercentJournalApp.swift
+//  OnePercentJournal
+//
+//  Created by Prachi Heda on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OnePercentJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
